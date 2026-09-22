68a4ae9df9602a138c6840e9bbe1c382698b7ef7
JBRagulkannan <ragulkannanjai1881999@gmail.com>
2026-09-22 12:40:16 +0530

    Update Defect_triage_process.py

--- Defect_triage_process.py
+++ Defect_triage_process.py
@@ -11,6 +11,7 @@ import tempfile
 import uuid
 import matplotlib.pyplot as plt
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode, JsCode
+JIRA_BASE_URL = st.secrets["JIRA_BASE_URL"]
 
 # --------------------------------------------------
 # Optional Outlook Integration
